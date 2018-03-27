4adc0c0a7ee2c75092169882b875509f63db14b6
Marian Zburlea <marianzburlea@gmail.com>
2018-03-27 11:15:23 +0100

    update spread operator

--- spread-operator.js
+++ spread-operator.js
@@ -2,7 +2,7 @@
 const paramList = ["code", true, 25]
 
 const otherList = [1, 2, ...paramList]
-// 1, 2, "code", true, 25
+// [1, 2, "code", true, 25]
 
 const f = (a, b, ...c)
         => (a + b) * c
@@ -10,6 +10,11 @@ f(3, 5, 'John', 'Ana', 'Maya') === 24
 
 const name = 'codetap'
 const charList = [ ...name ]
-// ["M", "a", "r", "i", "a", "n"]
+// ["c", "o", "d", "e", "t", "a", "p"]
+
+
+
+
+
 
 
