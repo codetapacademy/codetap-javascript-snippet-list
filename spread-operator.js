eca981be67d1dce1e19238bf2985ee4abfb5b312
Marian Zburlea <marianzburlea@gmail.com>
2018-03-27 11:17:15 +0100

    add missing length to the f function

--- spread-operator.js
+++ spread-operator.js
@@ -5,7 +5,7 @@ const otherList = [1, 2, ...paramList]
 // [1, 2, "code", true, 25]
 
 const f = (a, b, ...c)
-        => (a + b) * c
+        => (a + b) * c.length
 f(3, 5, 'John', 'Ana', 'Maya') === 24
 
 const name = 'codetap'
