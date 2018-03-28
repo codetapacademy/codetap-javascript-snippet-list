58af716200bed58d4f5ae6e8af23f2511460fe33
Marian Zburlea <marianzburlea@gmail.com>
2018-03-28 13:16:23 +0100

    Property shorthand

---
+++ property-shorthand.js
@@ -0,0 +1,25 @@
+// Property shorthand
+// ES6
+let a = 2,
+    name = 'codetap'
+const list = { a, name }
+
+// ES5
+var b = 3,
+    company = 'codetap';
+var collection = {
+    b: b,
+    company: company
+};
+
+
+
+
+
+
+
+
+
+
+
+
