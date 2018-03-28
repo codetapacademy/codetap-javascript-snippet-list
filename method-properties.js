abe4da5a14d24ae1c5f4de44a9817b7d4f36e1e1
Marian Zburlea <marianzburlea@gmail.com>
2018-03-29 00:43:40 +0100

    Method Properties

---
+++ method-properties.js
@@ -0,0 +1,17 @@
+// Method Properties
+// ES6
+const list = {
+    print(a, b) {
+        console.log(a + b)
+    }
+}
+list.print(2, 3) // 5
+
+// ES5
+var table = {
+    show: function (a, b) {
+        console.log(a + b)
+    }
+}
+
+table.show(4, 5) // 9
