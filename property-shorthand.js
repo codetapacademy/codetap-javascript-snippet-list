517edd9a753846cee92ff9ff52f5b600cefc3e5e
Marian Zburlea <marianzburlea@gmail.com>
2018-03-28 13:18:39 +0100

    update property shorthand with comments

--- property-shorthand.js
+++ property-shorthand.js
@@ -3,6 +3,7 @@
 let a = 2,
     name = 'codetap'
 const list = { a, name }
+// {a: 2, name: "codetap"}
 
 // ES5
 var b = 3,
@@ -11,6 +12,7 @@ var collection = {
     b: b,
     company: company
 };
+// {b: 3, company: "codetap"}
 
 
 
