6e62e5daaf8e0d22492d5e2a439e025cee911608
Marian Zburlea <marianzburlea@gmail.com>
2018-03-31 15:54:14 +0200

    Object and array matching with default values

---
+++ object-and-array-matching-with-default-values.js
@@ -0,0 +1,20 @@
+// Object and array matching with default values
+// ES6
+let name = {first: 'Marian'};
+let list = ['buys'];
+let {first, last = 'Zburlea'} = name;
+let [get, buy = 'milk'] = list;
+console.log(first, last, get, buy)
+// Marian Zburlea buys milk
+
+// ES5
+var info = {brand: 'CodeTap'};
+var brand = info.brand;
+var type = info.type || 'Eductation';
+var how = info.how || 'through';
+var other = ['boosts'];
+var action = other[0];
+var what = other[1] || 'career';
+var whos = other[2] || 'your';
+console.log(brand, action, whos, what, how, type)
+// CodeTap boosts your career through Eductation
